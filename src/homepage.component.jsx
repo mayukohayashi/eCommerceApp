5e6812d1d97521be9988f962152119542476ab55
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-09 15:55:21 +0900

    add: start app.js and sass

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <hi className='title'>HATS</hi>
+          <span className='subtitle'>shop now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <hi className='title'>JACKETS</hi>
+          <span className='subtitle'>shop now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <hi className='title'>SNEAKERS</hi>
+          <span className='subtitle'>shop now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <hi className='title'>MENS</hi>
+          <span className='subtitle'>shop now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <hi className='title'>WOMESN</hi>
+          <span className='subtitle'>shop now</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+)
+
+export default HomePage;
