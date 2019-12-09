405721a0848bb291998979db14b7baefc18453ee
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-09 17:37:29 +0900

    add: setting bottom row bigger with large size component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl }) => (
-  <div style={{
-    backgroundImage: `url(${imageUrl})`
-  }} className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className={`${size} menu-item`}
+  >
     <div className='content'>
       <hi className='title'>{title}</hi>
       <span className='subtitle'>shop now</span>
