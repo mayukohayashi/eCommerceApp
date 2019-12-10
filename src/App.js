3ad55f929f95c3f29a8b65084f53cf8afca8811c
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-10 16:10:20 +0900

    add: collection-preview and shop-page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 const HatsPage = () => (
   <div>
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
