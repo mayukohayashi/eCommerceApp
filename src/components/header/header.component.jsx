fa7535dedb793fb7652aada62cfb42ad6c00aaaa
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-11 01:04:37 +0900

    add: header page styled

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss'
+import './header.styles.scss';
 
 const Header = () => (
   <div className='header'>
-    <link className='logo-container' to="/">
+    <Link className='logo-container' to='/'>
       <Logo className='logo' />
-    </link>
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+    </div>
   </div>
-)
+);
+
+export default Header;
