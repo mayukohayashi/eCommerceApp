4807687e1db5587a6a2c7edbbf236ae248abfa8d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-11 15:44:01 +0900

    feat: styling and labeling signup component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component'
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -32,23 +34,23 @@ class SignIn extends React.Component {
         <span>Emailとパスワードを入力してください</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='email'
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name='password'
             type='password'
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='password'
             required
           />
-          <label>Password</label>
-      
+
           <input type='submit' value='Submit Form' />
         </form>
       </div>
