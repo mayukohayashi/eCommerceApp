4e72786fbb016fddcbea93400f966184f167fc92
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-11 15:47:35 +0900

    fix: signin whole component style as 30%

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -30,7 +30,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>すでにアカウントをお持ちの方はこちら</h2>
+        <h2>アカウントをお持ちの方はこちら</h2>
         <span>Emailとパスワードを入力してください</span>
 
         <form onSubmit={this.handleSubmit}>
