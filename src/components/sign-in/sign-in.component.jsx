b005dd5e6e02f89957cb43b14b29ad760ea933c9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-11 16:21:43 +0900

    feat: done with custom-button, todo: use for other buttons too

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
 import './sign-in.styles.scss';
 
@@ -51,7 +52,7 @@ class SignIn extends React.Component {
             required
           />
 
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'>Sign In</CustomButton>
         </form>
       </div>
     );
