8f76a5831b6348269a76a5f3ce996a50cbe1ffe8
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-12 15:23:46 +0900

    feat: storing user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,28 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if(!userAuth) return;
 
-  console.log(firestore.doc('users/123fdsfrr'))
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('Have error creating user', error.message)
+    }
+  }
+
+  return userRef;
+
 }
 
 firebase.initializeApp(config)
