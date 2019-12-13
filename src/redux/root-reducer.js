3cf3e8aed39719b1b233a1c181c0466425bc76cb
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-13 16:42:23 +0900

    feat: cart dropdown worked

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
-import caartReducer from './cart/cart.reducer';
 import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
