403c4b3c2dc10c25a1c9a0f6f8e318edfbf34ac9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-13 15:46:03 +0900

    add: setting cart-dropdown component basic

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component'
+
 import './cart-dropdown.styles.scss'
+
+const CartDropdown = () => (
+  <div className='cart-dropdown'>
+    <div className='cart-item' />
+    <CustomButton>お支払い</CustomButton>
+  </div>
+)
+
+export default CartDropdown
