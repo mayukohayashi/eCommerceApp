56a2e814ade5dd9bd557c91c027eff6e8e4f0a78
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-14 19:00:00 +0900

    feat: click BUY NOW button and fire toggleCartHidden

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -22,7 +23,12 @@ const CartDropdown = ({cartItems, history}) => (
           )
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')} >BUY NOW</CustomButton>
+    <CustomButton onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden())
+      }}>
+        BUY NOW
+      </CustomButton>
   </div>
 )
 
