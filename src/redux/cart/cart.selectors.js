ebb556de9948ed64f99fa3c7b8e91c98aecc6dc0
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-14 18:11:39 +0900

    add: set checkout page with router, selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,6 +17,17 @@ export const selectCartItemsCount = createSelector(
   cartItems =>
     cartItems.reduce(
       (accumulatedQuantity, cartItem) =>
-      accumulatedQuantity + cartItem.quantity , 0
+      accumulatedQuantity + cartItem.quantity,
+      0
     )
+)
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) =>
+    accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 )
