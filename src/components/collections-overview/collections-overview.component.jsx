995e3bb3dee16761213c58331bb67a1f1a44d1c6
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-16 16:05:19 +0900

    feat: created data flow into collections

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview  from '../collection-preview/collection-preview.component'
-import { selectCollections } from '../../redux/shop/shop.selector';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 
 import './collections-overview.styles.scss';
 
@@ -16,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
