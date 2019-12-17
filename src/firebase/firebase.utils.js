1f7971614de009a17c096ef12030d319cd574fbd
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-17 21:09:22 +0900

    add: prepare try to store collections into firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,7 +17,6 @@ firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
-
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
@@ -40,11 +39,18 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const addCollectionAndDocuments = (collectionKey, objectToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
