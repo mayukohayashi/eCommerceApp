bb4e62d5d60b3cd91dc845296d8f8c5cf36fc7ec
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-18 21:36:29 +0900

    try: one of the example getting api from firebase with .fetch (too nested)

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,13 +28,17 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.get().then(
-      this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({loading: false});
-      })
-    )
+    fetch('https://firestore.googleapis.com/v1/projects/stoneshopdb/databases/(default)/documents/collections')
+    .then(response => response.json())
+    .then(collections => console.log(collections))
+
+    // collectionRef.get()
+
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({loading: false});
+    // })
   }
 
   render() {
