225b196dc1969f91efc019657fa6c627aa1f9f86
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-18 18:34:37 +0900

    feat: sending collection page to null either

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -14,7 +14,6 @@ export const selectCollectionsForPreview = createSelector(
 );
 
 export const selectCollection = collectionUrlParam =>
-  createSelector(
-    [selectCollections],
-    collections => collections[collectionUrlParam]
+  createSelector([selectCollections], collections =>
+    collections ? collections[collectionUrlParam] : null
   );
