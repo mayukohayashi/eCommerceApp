462453523b17754d675abf86d35bed88171846e7
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-18 21:24:04 +0900

    try: one of the way to set Promises

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,11 +28,13 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({loading: false});
-    })
+    collectionRef.get().then(
+      this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+        this.setState({loading: false});
+      })
+    )
   }
 
   render() {
