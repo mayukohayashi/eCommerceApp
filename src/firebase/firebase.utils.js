5ed10f2fb049906d3e4ad301b5fffa37ae27a2b4
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-18 17:50:03 +0900

    feat: set collection data into the routint

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -52,6 +52,24 @@ export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = collectionSnapshot => {
+  const transformedCollection = collectionSnapshot.docs.map(docSnapshot => {
+    const { title, items } = docSnapshot.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: docSnapshot.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
