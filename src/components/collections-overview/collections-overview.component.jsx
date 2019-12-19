9f63a214d9c51272b3d4e5fb0cdfb9136955c084
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-19 17:16:13 +0900

    reafac: collection-overview styled by styled container

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,16 +4,15 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
-
-import './collections-overview.styles.scss';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
 const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
+  <CollectionsOverviewContainer>
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-  </div>
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
