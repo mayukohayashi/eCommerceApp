6d802a22ff352d724e23a667f8a9c67da776a796
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-20 17:15:18 +0900

    add: setting user reducer for email and google sign-in failure with saga

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,7 +1,8 @@
 import { UserActionTypes } from './user.types';
 
 const INITIAL_STATE = {
-  currentUser: null
+  currentUser: null,
+  error: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
@@ -10,9 +11,16 @@ const userReducer = (state = INITIAL_STATE, action) => {
     case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
       return {
         ...state,
-        currentUser: action.payload
+        currentUser: action.payload,
+        error: null
       };
 
+    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+    case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
