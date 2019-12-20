b7743319ad4822d52f170f5ebf1645ca5cacf8cb
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-20 14:44:43 +0900

    feat: set shop saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import createSagaMifflrware from 'redux-saga';
+import createSagaMiddlerware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddlerware();
+
+const middlewares = [];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
+
 export const persistor = persistStore(store);
 
 export default { store, persistor };
