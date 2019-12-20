9d4a0da33bb188331af3b8ba52ccd41f7f3ca3a9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-20 14:17:22 +0900

    add: install redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import createSagaMifflrware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
