7cb4683bd87215160b06ff99d28a690b58056e65
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-22 18:22:32 +0900

    feat: set port5000 to backend and connect to react side too

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
