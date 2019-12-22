de51ef6095d41dcfededcb895c639918cc02a928
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-22 15:00:04 +0900

    learn: leaning custom hooks, in user and post

--- _learning_reactHooks/custom-hook-example/src/components/post/post.component.jsx
+++ _learning_reactHooks/custom-hook-example/src/components/post/post.component.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../card/card.component';
 
-import useFetch from '../../effects/use-fetch.effect';
-
 const Post = ({ postId }) => {
-  const post = useFetch(
-    `https://jsonplaceholder.typicode.com/posts?id=${postId}`
-  );
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async() => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${postId}`)
+      const posts = await res.json();
+      setPost(posts[0]);
+    }
+
+    fetchPost();
+  })
 
   return (
     <Card>
--- _learning_reactHooks/custom-hook-example/src/components/user/user.component.jsx
+++ _learning_reactHooks/custom-hook-example/src/components/user/user.component.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Card from '../card/card.component';
 
-import useFetch from '../../effects/use-fetch.effect';
-
 const User = ({ userId }) => {
-  const user = useFetch(
-    `https://jsonplaceholder.typicode.com/users?id=${userId}`
-  );
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async() => {
+      const res = fetch (`https://jsonplaceholder.typicode.com/users?id=${userId}`)
+      const users = await (await res).json();
+      setUser(users[0])
+    }
+
+    fetchUser()
+  })
 
   return (
     <Card>
