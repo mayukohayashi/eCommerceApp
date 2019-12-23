e694f3f48c750ac25fd53387cb53c54384271914
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-23 14:21:14 +0900

    feat: POST to stripe server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -3,28 +3,28 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price;
-  const publishableKey = 'pk_test_dtqeYK95FJz5ewmFTRc6qaNQ00j2VICd8D'
+  const publishableKey = 'pk_test_dtqeYK95FJz5ewmFTRc6qaNQ00j2VICd8D';
 
   const onToken = token => {
     console.log(token);
-    alert('Paymen Successful')
-  }
+    alert('Paymen Successful');
+  };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='Stoned Shop Ltd.'
+      label="Pay Now"
+      name="Stoned Shop Ltd."
       currency="JPY"
       billingAddress
       shippingAddress
-      image='https://sendeyo.com/up/d/f3eb2117da'
+      image="https://sendeyo.com/up/d/f3eb2117da"
       description={`Your total is ¥${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
-  )
-}
+  );
+};
 
-export default StripeCheckoutButton
+export default StripeCheckoutButton;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,7 +18,7 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
@@ -24,4 +26,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('server running on port');
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'jpy'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
