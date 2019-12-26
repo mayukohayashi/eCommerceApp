fdbb5bc75d9b4ceabfdb88300a19782e16bef119
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-26 19:02:47 +0900

    feat: mobile friendly for header and content

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -1,9 +1,13 @@
-import {} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Open Sans Condensed', 'Kosugi Maru';
     padding: 20px 60px;
+
+    @media screen and (max-width: 800px) {
+      padding: 10px;
+    }
   }
 
   a {
