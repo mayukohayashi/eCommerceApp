2056488516ff1ebc583708b4dde323ea36469a1e
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-23 21:48:23 +0900

    lern: lerning how redux test work

---
+++ client/src/redux/cart/cart.reducer.test.js
@@ -0,0 +1,11 @@
+import CartActionTypes from './cart.types';
+import cartReducer from './cart.reducer';
+
+const initialState = {
+  hidden: true,
+  cartItems: []
+};
+
+describe('cartReducer', () => {
+
+})
